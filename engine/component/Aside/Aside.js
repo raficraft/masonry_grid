0cd4dd774b8e8f0663ae27877b014a5c8682d115
raficraft <raficraft@gmail.com>
2022-03-18 10:25:23 +0100

    add pages

--- engine/component/Aside/Aside.js
+++ engine/component/Aside/Aside.js
@@ -1,8 +1,7 @@
-import React, { useLayoutEffect } from "react";
+import React from "react";
 
 import S from "./Aside.module.scss";
 import { useClickOutside } from "../../hooks/useClickOutside";
-import useMediaQuery from "../../hooks/useMediaQueries";
 
 export default function Aside({ children }) {
   const [show, setShow, refOutsideClick] = useClickOutside(false);
